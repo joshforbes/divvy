ec62584bef46be8ace22b9e272d791abccc0ea3a
Josh Forbes <jdforbes82@gmail.com>
2015-02-15 14:52:08 -0500

    styling the splash page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Divvy</title>
 
-	<link href="/css/app.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-	<link href='//fonts.googleapis.com/css?family=Leckerli+One' rel='stylesheet' type='text/css'>
+    <!-- Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Leckerli+One' rel='stylesheet' type='text/css'>
 
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-	@include('layouts.partials.nav')
-	<div class="container">
-		@yield('content')
-	</div>
+@include('layouts.partials.nav')
 
-	<!-- Scripts -->
-	<script src="/js/vendor/jquery.js"></script>
-	<script src="/js/vendor/bootstrap.js"></script>
-	<script src="/js/vendor/handlebars.js"></script>
-	<script src="/js/app.js"></script>
+@yield('content')
+
+
+<!-- Scripts -->
+<script src="/js/vendor/jquery.js"></script>
+<script src="/js/vendor/bootstrap.js"></script>
+<script src="/js/vendor/handlebars.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
--- resources/views/pages/splash.blade.php
+++ resources/views/pages/splash.blade.php
@@ -1,8 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
+    <section class="splash-signup-section">
+        <div class="container-small">
+            <div class="signup-wrapper">
 
-    </div>
+                <div class="text-wrapper">
+                    <h1>Lorem ipsum dolor sit amet</h1>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab adipisci alias ex
+                        facilis in incidunt rem saepe tempore temporibus.</p>
+                </div>
+
+                <div class="registration-wrapper">
+                    {!! Form::open(['url' => '/auth/register']) !!}
+
+                    <!-- Username Form Input -->
+                    <div class="form-group">
+                        {!! Form::text('username', null, ['placeholder' => 'Username', 'class' => 'form-control']) !!}
+                    </div>
+
+                    <!-- Email Form Input -->
+                    <div class="form-group">
+                        {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control']) !!}
+                    </div>
+
+                    <!-- Password Form Input -->
+                    <div class="form-group">
+                        {!! Form::password('password', ['placeholder' => 'Create a password', 'class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::submit('Sign up for Divvy', ['class' => 'btn btn-success form-control']) !!}
+                    </div>
+
+                    {!! Form::close() !!}
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 @endsection
