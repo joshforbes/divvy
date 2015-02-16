0042ecf3970603d65dd6d1d5aed3084d07736141
Josh Forbes <jdforbes82@gmail.com>
2015-02-15 19:54:38 -0500

    created migrations for projects, comments, tasks, attachments, and pivot tables

---
+++ database/migrations/2015_02_16_002917_create_tasks_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tasks', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('project_id')->unsigned();
+			$table->integer('user_id')->unsigned();
+			$table->string('name');
+			$table->text('description');
+			$table->timestamps();
+
+			$table->foreign('project_id')
+				->references('id')
+				->on('projects')
+				->onDelete('cascade');
+
+			$table->foreign('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tasks');
+	}
+
+}
--- database/migrations/2015_02_16_003716_create_attachments_table.php
+++ database/migrations/2015_02_16_003716_create_attachments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateAttachmentsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,14 +12,17 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('attachments', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('username')->unique();
-			$table->string('email')->unique()->index();
-			$table->string('password', 60);
-			$table->rememberToken();
+			$table->integer('task_id')->unsigned();
+			$table->string('path');
 			$table->timestamps();
+
+			$table->foreign('task_id')
+				->references('id')
+				->on('tasks')
+				->onDelete('cascade');
 		});
 	}
 
@@ -30,7 +33,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('attachments');
 	}
 
 }
