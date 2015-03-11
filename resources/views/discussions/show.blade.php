2ae4d2969a7b8ea383f4d1b5a4d1417188104867
Josh Forbes <jdforbes82@gmail.com>
2015-03-11 08:04:50 -0400

    Working on the discussions view

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -1,1 +1,12 @@
-{{ dd($discussion) }}
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="discussion-wrapper">
+            @include('discussions.partials.discussion')
+        </div>
+    </div>
+
+
+@endsection
