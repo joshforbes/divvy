db9cd01d47a09be19d1f396295d2bcb26357b451
Josh Forbes <jdforbes82@gmail.com>
2015-05-04 22:50:21 -0400

    sprucing up the forgot password email

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,1 +1,8 @@
-Click here to reset your password: {{ url('password/reset/'.$token) }}
+<img style="border: 1px solid transparent; border-radius: 5px; box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);" src="{{ asset('images/divvy-email.jpg') }}" alt="Divvy Logo"/>
+
+<p>We received a request to reset your password.</p>
+Click here to reset your password:
+
+<a style="padding: 5px 15px; background-color: #184D59; color:white; text-decoration: none;" href="{{ url('password/reset/'.$token) }}">Reset</a>
+
+
